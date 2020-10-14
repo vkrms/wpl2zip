7e21f2ac2b1577958593b46add12dc2a17641f28
vkrms <april504@yandex.ru>
2020-10-14 12:32:07 +1000

    initial commit

---
+++ inc/classes/Parser.php
@@ -0,0 +1,34 @@
+<?php
+namespace Vkrms\Wpl2zip;
+
+use Laravie\Parser\Xml\Reader;
+use Laravie\Parser\Xml\Document;
+
+class Parser
+{
+    public function __construct(string $inputPath, string $outputFolder)
+    {
+        // create folder
+        if (!is_dir($outputFolder)) {
+            mkdir($outputFolder, 0755);
+        }
+
+        // parse wpl
+        $wpl = (new Reader(new Document()))->load($inputPath);
+        $parsed = $wpl->parse([
+            'media' => ['uses' => 'body.seq.media[::src>src]'],
+        ]);
+
+        foreach ($parsed['media'] as $media) {
+            $path = str_replace('..', 'G:\Music', $media['src']);
+            $paths[] = $path;
+            $pathChunks = explode('\\', $path);
+            $filename = array_pop($pathChunks);
+            echo copy($path, $outputFolder.'\\'.$filename) ? "$path copied \r\n" : "copy failed on $path \r\n";
+        }
+
+        // print_r($paths);
+
+        return;
+    }
+}
---
+++ main.php
@@ -0,0 +1,9 @@
+<?php
+require './vendor/autoload.php';
+
+use Vkrms\Wpl2zip\Parser;
+
+$playlist = $argv[1];
+$folder   = $argv[2];
+
+new Parser($playlist, $folder);
